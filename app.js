c8ffa4e768048126a5ee3b42488900d3eea6c72a
marcosV1234321 <50997452+marcosV1234321@users.noreply.github.com>
2025-12-21 23:20:51 -0300

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-/* MASTER FLIX - LOG DE ATUALIZAÇÕES FUTURAS 
-   Este arquivo será usado para injetar funções novas via script tag.
-*/
-
-console.log("Sistema de updates pronto para novas ordens, Master.");
-
-// Exemplo de como adicionar um botão novo via código futuro:
-// document.querySelector('header').insertAdjacentHTML('beforeend', '<button>Nova Função</button>');
